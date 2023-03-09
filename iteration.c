488ac2070d6edc4fc8641f5021c7a986350694a6
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-03-10 01:29:11 +0530

    Create iteration.c

---
+++ iteration.c
@@ -0,0 +1,17 @@
+/*
+output: Jay Ganesh multiple times
+Date: 19/02/23
+Author: Tanmayee Kadam */
+
+#include<stdio.h>
+
+int main()
+{
+register int iCnt=0;
+
+for(iCnt=1;iCnt<=4;iCnt++)
+{
+printf("Jay Ganesh\n");
+}
+return 0;
+}
