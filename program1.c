304e6a4a6589694fa21400238964fdc44ca78599
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-19 18:34:05 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,13 @@
+/*
+Function name: Jay GAnesh
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :C programming
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Jay Ganesh\n");
+    return 0;
+}
