ed566c8a88cf41876f25e5096e137ea3b42945ce
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-25 22:02:15 +0530

    Add files via upload

---
+++ program5helper.c
@@ -0,0 +1,20 @@
+/*
+Function Name:Addition
+input: unsigned integer, unsigned integer
+output: unsigned integer
+Description: addition of 2 numbers
+Author: Tanmayee Kadam
+Date: 19 April 2023
+
+*/
+
+#include"program5header.h"
+
+unsigned int Addition(unsigned int Value1, unsigned int Value2)//prototype
+{
+    unsigned int iResult = 0;
+
+    iResult = Value1 + Value2;
+
+    return iResult;
+}
