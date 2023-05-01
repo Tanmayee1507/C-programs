2e029f69f4447ffb27707835a3bbe6fecf9ecb00
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-02 00:10:21 +0530

    Add files via upload

---
+++ program9.c
@@ -0,0 +1,19 @@
+/*
+Function Name:Sequence
+output: JAy GAnesh
+Description: display jay Ganesh
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:C Programming
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    return 0;
+}
