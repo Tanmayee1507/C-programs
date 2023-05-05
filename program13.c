f35d69f6879e1ffb7c89bd6a432f7a3add0db5da
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-06 00:56:06 +0530

    Add files via upload

---
+++ program13.c
@@ -0,0 +1,37 @@
+/*
+Function Name:Iteration using filter
+input: integer
+output: Jay ganesh
+Description: Display Jay Ganesh 
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:C Programming
+*/
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)  //filter
+    {
+        printf("Please enter the positive number\n");
+        return;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt ++)
+    {
+        printf("Jay Ganesh... \n");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter th number of times to Display string:\n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
