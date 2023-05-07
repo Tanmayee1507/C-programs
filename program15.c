9f2e9c63135ec53a6a56fa344f46f4ccf4b0da2c
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-07 22:39:46 +0530

    Add files via upload

---
+++ program15.c
@@ -0,0 +1,20 @@
+/*
+Function Name:Sequence
+input: integer
+output: 1-5
+Description:Display 1-5 on screen
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:C Programming
+*/
+#include<stdio.h>
+int main()
+{
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+
+    return 0;
+}
