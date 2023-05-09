be83837bf00a1cc03bc585d0528f8cfb22f47bf3
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-09 23:30:07 +0530

    Add files via upload

---
+++ program16.c
@@ -0,0 +1,23 @@
+/*
+Function Name:iteration
+input: integer
+output: 1-5
+Description:Display 1-5 on screen
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:C Programming
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 5; iCnt ++)
+    {
+        printf("%d\n",iCnt);
+    }
+    return 0;
+
+}
