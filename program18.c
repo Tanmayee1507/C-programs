a632686f559d8cb9c1caed97607b03b13943543c
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-10 22:49:22 +0530

    Add files via upload

---
+++ program18.c
@@ -0,0 +1,28 @@
+/*
+Function Name:iteration
+input: integer
+output: 1-5
+Description:Display 1-5 on screen
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:C Programming
+*/
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt ++)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter thr number:\n");
+    scanf("%d",&iValue);
+    printf("Frequency is:\n");
+    Display(iValue);
+    return 0;
+
+}
