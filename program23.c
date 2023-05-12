096c643b2d1fead87d41e59b4c6d04a65eca2848
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-12 20:15:04 +0530

    Add files via upload

---
+++ program23.c
@@ -0,0 +1,35 @@
+/*
+Function Name:Problems on numbers
+output: 
+Description: Display factors of the number
+Author: Tanmayee K Deepak
+Date:2 May 2023
+Language:C Programming
+*/
+#include<stdio.h>
+
+void DisplayFactors(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo%iCnt)==0)
+        {
+
+        printf("%d \n",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter thr value:\n");
+    scanf("%d", &iValue);
+
+    DisplayFactors(iValue);
+
+
+    return 0;
+}
