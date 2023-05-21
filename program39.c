665f02eb6b1dd696ecd0b43d1d9da4a3eb243a17
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-21 23:02:12 +0530

    Add files via upload

---
+++ program39.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+    int iCnt = 0;
+    int iAns = 1;
+    for(iCnt=1; iCnt<= iNo; iCnt++)
+    {
+        iAns=iCnt*iAns;
+    }
+    return iAns;
+}
+int main()
+{
+int iValue = 0;
+int iRet = 0;
+printf("Enter the value:\n");
+scanf("%d", &iValue);
+
+iRet = Factorial(iValue);
+printf("Result is:%d\n",iRet);
+return 0;
+}
