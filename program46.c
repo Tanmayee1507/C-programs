09c293125ebcfc87d971334895aff4db5488447a
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-25 21:14:42 +0530

    Add files via upload

---
+++ program46.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CalculatePower(int iBase, int iPower)
+{
+    int iCnt = 0;
+    int iResult = 1;
+    iCnt = 1;
+    while(iCnt <= iPower)
+    {
+        iResult = iResult*iBase;
+        iCnt++;
+  
+    }
+        return iResult;
+
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    int iRet = 0;
+    printf("Enter the base number:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the power number:\n");
+    scanf("%d",&iValue2);
+    iRet = CalculatePower(iValue1, iValue2);
+
+    printf("Result is:%d \n", iRet);
+    
+   
+    return 0;
+
+}
