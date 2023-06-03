bc7833f66a5a56ba847b9df0576f192401ec83ad
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-06-03 23:49:04 +0530

    Add files via upload

---
+++ program67.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int SmallestDigit(int iNo)
+{
+    int iDigit = 0;
+    int iCnt = 0;
+    int iMin = 9;
+    
+    if(iNo<0)
+    {
+        iNo= -iNo;
+    }
+
+    while(iNo!=0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit<iMin)
+        {
+            iMin = iDigit;
+        }
+        iNo = iNo/10;
+    }
+    return iMin;
+   
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet  = 0;
+
+    printf("Enter the Number:\n");
+    scanf("%d",&iValue);
+
+    iRet = SmallestDigit(iValue);
+    printf("Smallest of Digits is :%d\n", iRet);
+
+    return 0;
+}
