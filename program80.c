611e74d3d7dfa719acc692b60abf57415f8c3d07
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-06-09 20:57:15 +0530

    Add files via upload

---
+++ program80.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int OddCount(int Arr[], int iLength)
+{
+    int iCnt = 0;
+}
+
+int main()
+{
+    int iSize = 0;
+    int iCnt=0;
+    int *ptr = NULL;
+    printf("Enter the number elements:\n");
+    scanf("%d",&iSize);
+
+ptr=(int*)malloc(iSize * sizeof(int));
+printf("Enter the elements of array\n");
+
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+    scanf("%d",&ptr[iCnt]);
+    }
+
+    printf("Elements of array are:\n");
+    for(iCnt= 0; iCnt<iSize;iCnt++)
+    {
+    printf("%d\n",ptr[iCnt]);
+    }
+
+    return 0;
+}
