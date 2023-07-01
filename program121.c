53c66524e86b45526ea1ad2a39f079c7d8e4388a
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-01 23:09:50 +0530

    Add files via upload

---
+++ program121.c
@@ -0,0 +1,45 @@
+/*
+output: 1   1   1   1
+        *   *   *   *
+        3   3   3   3
+        *   *   *   *
+*/
+#include<stdio.h>
+void Display(int iRow, int iCol)
+{
+   int i = 0;
+   int j = 0;
+   int iNo = 0;
+   
+ for(i = 1; i <=iRow ; i++)//outer loop
+{
+    for(j =1; j<=iCol; j++)//inner loop
+    {
+        if((i%2)==0) 
+        {
+    printf("*\t", iRow);
+        }
+        else
+        {
+            printf("%d\t",i);
+            
+        }
+    
+    }
+    printf("\n");
+}
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    printf("Enter row size: \n");
+    scanf("%d",&iValue1);
+     printf("Enter column size: \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
