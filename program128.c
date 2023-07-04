c9737c94e0a842ceaffea2857a78a7ac90e149ab
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-04 22:30:17 +0530

    Add files via upload

---
+++ program128.c
@@ -0,0 +1,51 @@
+/*
+output: *   *   *   *   *   *
+        *   *   %   %   %   *
+        *   %   *   %   %   *  
+        *   %   %   *   %   *
+        *   %   %   %   *   *
+        *   *   *   *   *   *
+*/
+#include<stdio.h>
+void Display(int iRow, int iCol)
+{
+   int i = 0;
+   int j = 0;
+   int iNo = 0;
+   if(iRow !=iCol)
+   {
+    printf("Invalid output\n");
+    return ;
+   }
+//1     1           2      3
+   for(i = 1; i <= iRow; i++)//outer loop
+{
+    for(j =1; j<=iCol; j++)//inner loop
+    {
+        if((i == 1) || (i == iRow) || (i == j) || ( j == iCol) || (1 == j) )
+        {
+    printf("*\t", iCol);
+        }
+        else
+        {
+            printf("&\t",i);
+        }
+    
+    }
+    printf("\n");
+}
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    printf("Enter row size: \n");
+    scanf("%d",&iValue1);
+     printf("Enter column size: \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
