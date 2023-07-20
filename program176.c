1be25dfb69a7bc0bb9fdeb123f2303a4d83a2e16
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-20 22:59:14 +0530

    Add files via upload

---
+++ program176.c
@@ -0,0 +1,27 @@
+//replace spaces by _
+#include<stdio.h>
+void EditString(char *str)
+{
+    while(*str!='\0')
+    {
+        if(*str==' ')
+        {
+            *str='_';
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("ENter string:\n");
+    scanf("%[^\n]s", Arr);
+    EditString(Arr);
+
+    printf("String after editing is: %s\n", Arr);
+
+    return 0;
+}
+
