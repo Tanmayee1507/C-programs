4368953a3a47fe44301f1d4489beb65399ecb4b5
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-23 21:53:15 +0530

    Add files via upload

---
+++ program185.c
@@ -0,0 +1,33 @@
+//copy string from
+// DEMO\0  madhla \0 pn copy vhayla hava ani he fakta programmer la kalta user la nahi
+#include<stdio.h>
+void StrcpycapX(char *src, char *dest)
+{
+    
+    while(*src!='\0')
+    {
+        if((*src>='A')&& (*src<='Z'))
+        {
+        *dest = *src;
+        dest++; // inside if to avoid white boxes/spaces
+        }
+        src++;
+        
+    }
+    *dest = '\0'; // for \0
+}
+
+int main()
+{
+    char Arr[20];
+    char Brr[20];
+
+    printf("ENter string:\n");
+    scanf("%[^\n]s", Arr);
+    StrcpycapX(Arr, Brr);
+
+    printf("String after copy is: %s\n", Brr);
+
+    return 0;
+}
+
