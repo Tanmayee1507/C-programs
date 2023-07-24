271b19e82479b2b254203dc81f5b869e83a29c1b
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-24 22:43:18 +0530

    Add files via upload

---
+++ program189.c
@@ -0,0 +1,33 @@
+//copy string after prev string a.k.a concatenation
+// DEMO\0  madhla \0 pn copy vhayla hava ani he fakta programmer la kalta user la nahi
+#include<stdio.h>
+void StrcatX(char *src, char *dest)
+{
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+    while(*src != '\0')
+    {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+    *dest = '\0';
+}
+
+int main()
+{
+    char Arr[20];
+    char Brr[20]= "Demo";
+
+    printf("ENter string:\n");
+    scanf("%[^\n]s", Arr);
+    
+    StrcatX(Arr, Brr);
+
+    printf("String after copy is: %s\n", Brr);
+
+    return 0;
+}
+
