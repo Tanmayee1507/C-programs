39fa939b0674ee31c1ec3bed23fda3782f57f56e
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-28 21:50:02 +0530

    Add files via upload

---
+++ program201.c
@@ -0,0 +1,54 @@
+//check if string is pallindrome
+//str1 Amma = Amma usingflag
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPallindrome(char *str)
+{
+    char *start = NULL;
+    char *end = NULL;
+    bool bFlag = true;
+    start = str;
+    end = str;
+
+    while(*end !='\0')
+    {
+        end++;
+
+    }
+    end--;//  \0 cha ek ghar alikade yenya sathi
+    
+    while(start < end)
+    {
+        if(*start != *end)
+        {
+            bFlag = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+    return bFlag;
+
+}
+
+int main()
+{
+    char Arr[20];
+    bool bRet= false;
+    printf("Enter the string:\n");
+    scanf("%[^'\n']s",Arr);
+
+    bRet=  CheckPallindrome(Arr);
+
+if(bRet== true)
+{
+        printf("String is pallindrome\n");
+}
+else
+{
+    printf("String is not pallindrome\n");
+}
+
+    return 0;
+}
