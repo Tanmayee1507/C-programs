37cbeb93ac58bfd08b6482a721435538b4a8ec5b
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2024-04-16 23:50:54 +0530

    Add files via upload

--- array2.c
+++ array2.c
@@ -15,12 +15,3 @@ printf("%d",&Arr[1]);
 printf("%d",&Arr[2]);
 }
 
-/*array2.c: In function 'main':
-array2.c:4:20: warning: excess elements in array initializer
- int Arr[2] ={10,20,30,40};
-                    ^~
-array2.c:4:20: note: (near initialization for 'Arr')
-array2.c:4:23: warning: excess elements in array initializer
- int Arr[2] ={10,20,30,40};
-                       ^~
-array2.c:4:23: note: (near initialization for 'Arr')*/
